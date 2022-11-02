c21e120bd434b4033593b0816a7e97e0e1872587
Stan Pers <stanislav.kad@gmail.com>
2022-11-02 22:33:34 +0400

    login done

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
     mongoURI: 'mongodb+srv://StanKad:MEAN007@cluster0.a17se7i.mongodb.net/?retryWrites=true&w=majority',
+    jwtSecret: 'shampoo',
 }
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,32 @@
 const bcrypt = require('bcryptjs');
-const User = require('../models/User')
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+const keys = require('../config/keys');
 
-module.exports.login = (req, res) => {
-    res.status(200).json({
-        login: {
-            email: req.body.email,
-            pass: req.body.pass,
-        },
-    })
+module.exports.login = async (req, res) => {
+    const candidate = await User.findOne({ email: req.body.email });
+    if (!candidate) {
+        res.status(404).json({
+            message: 'User not found, please register!'
+        })
+    } else {
+        const passwordResult = bcrypt.compareSync(req.body.password, candidate.password)
+        if (passwordResult) {
+            const token = jwt.sign({
+                email: candidate.email,
+                userId: candidate._id, 
+            }, keys.jwtSecret, {expiresIn: 60 * 60});
+            res.status(200).json({
+                token: `Bearer ${token}`, 
+                message: 'You are logged in!'
+            })
+        } else {
+            res.status(401).json({
+                message: 'Password incorrect, try again!'
+            })
+        }
+    }
+
 }
 
 module.exports.register = async (req, res) => {
