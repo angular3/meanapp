a202d6d51a29a9e4e392cd4d331a6e3930ffe4a3
Stan Pers <stanislav.kad@gmail.com>
2022-12-14 00:00:23 +0100

    order page was created

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,3 +1,5 @@
+import { OrderPositionsComponent } from './order-page/order-positions/order-positions.component';
+import { OrderCategoriesComponent } from './order-page/order-categories/order-categories.component';
 import { CategoriesFormComponent } from './categories-page/categories-form/categories-form.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router'; // navigation between pages
@@ -41,7 +43,11 @@ const routes: Routes = [ // we created two layouts(common comps) and they will b
       {path: 'overview', component: OverviewPageComponent},
       {path: 'analytics', component: AnalyticsPageComponent},
       {path: 'history', component: HistoryPageComponent},
-      {path: 'order', component: OrderPageComponent},
+      {
+        path: 'order', component: OrderPageComponent, children: [ // here we pass two sub conpotetnts for two sub-pages
+          { path: '', component: OrderCategoriesComponent },
+          { path: ':id', component: OrderPositionsComponent }
+      ]},
       {path: 'categories', component: CategoriesPageComponent},
       {path: 'categories/new', component: CategoriesFormComponent},
       {path: 'categories/:id', component: CategoriesFormComponent},
--- client/src/app/order-page/order-page.component.ts
+++ client/src/app/order-page/order-page.component.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { OrderService } from './order.service';
+import { NavigationEnd, Router } from '@angular/router';
+import { Component, OnInit, ViewChild, ElementRef, OnDestroy, AfterViewInit } from '@angular/core';
+import { MaterialInstance, MaterialService } from '../shared/classes/material.service';
 
 @Component({
   selector: 'app-order-page',
   templateUrl: './order-page.component.html',
-  styleUrls: ['./order-page.component.css']
+  styleUrls: ['./order-page.component.css'],
+  providers: [OrderService]
 })
-export class OrderPageComponent implements OnInit {
+export class OrderPageComponent implements OnInit, OnDestroy, AfterViewInit {
 
-  constructor() { }
+  @ViewChild('modal') modalRef?: ElementRef
+  modal!: MaterialInstance;
+  isRoot: boolean = true;
+
+  constructor(private router: Router, private order: OrderService) { }
 
   ngOnInit(): void {
+    // because here we need to check all the time what exactely router is and change header every time 
+    // - we want to subscribe to router events stream
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        //  we have pretty much events when routes changing,
+        //  so here we change our variable only once in the end
+        this.isRoot = !this.router.url.includes('/order/');
+      }
+    })
   }
 
+  ngOnDestroy(): void {
+    this.modal.destroy?.();
+  }
+
+  ngAfterViewInit(): void {
+    this.modal = MaterialService.initModal(this.modalRef!)
+  }
+
+  onOpen() {
+    this.modal.open?.();
+  }
+
+  onCancel() {
+    this.modal.close?.();
+  }
+
+  onSubmit() {
+    this.modal.close?.();
+  }
 }
