ebc02d897d02c37147657f50bb85abb1ec1684cb
Stan Pers <stanislav.kad@gmail.com>
2022-12-25 22:38:26 +0100

    analytics controller on backend was updated

--- controllers/analytics.js
+++ controllers/analytics.js
@@ -1,7 +1,85 @@
-module.exports.overview = (req, res) => {
+const moment = require('moment');
+const Order = require('../models/Order');
+const errorHandler = require('../utils/errorHandler');
+
+
+errorHandler
+
+module.exports.overview = async (req, res) => {
+    try {
+        const allOrders = await Order.find({ user: req.user.id }).sort(1);
+        const ordersMap = getOrdersMap(allOrders);
+        const yesterdaysOrders = ordersMap[moment().add(-1, 'd').format('DD.MM.YYYY')] || []; // here we add -1 day to current one
+        const yesterdaysOrdersCount = yesterdaysOrders.length;
+
+        // count of all orders:
+        const allOrdersCount = allOrders.length;
+        // count of all working days(when we have orders):
+        const allDaysCount = Object.keys(ordersMap).length;
+        // oprders per day:
+        const ordersPerDay = (allOrdersCount / allDaysCount).toFixed(0);
+        // percent of orders count
+        const ordersPercent = (((yesterdaysOrdersCount / ordersPerDay) - 1) * 100).toFixed(2);
+        // total gain:
+        const totalGain = calculatePrice(allOrders);
+        // gain per day: 
+        const gainPerDay = totalGain / allDaysCount;
+        // gain for yesterday:
+        const yesterdaysGain = calculatePrice(yesterdaysOrders);
+        //gain percent:
+        const gainPercent = (((yesterdaysGain / gainPerDay) - 1) * 100).toFixed(2);
+        // compare our gain:
+        const compareGain = (yesterdaysGain - gainPerDay).toFixed(2);
+        //compare numbers of orders:
+        const compareOrdersNumbers = (yesterdaysOrdersCount - ordersPerDay).toFixed(2);
+
+        res.status(200).json({
+            gain: {
+                precent: Math.abs(+gainPercent),
+                compare: Math.abs(+compareGain),
+                yesterday: +yesterdaysGain,
+                isHigher: gainPercent > 0,
+            }, 
+            orders: {
+                precent: Math.abs(+ordersPercent),
+                compare: Math.abs(+compareOrdersNumbers),
+                yesterday: +yesterdaysOrders,
+                isHigher: ordersPercent > 0,
+            }
+        })
+    } catch (e) {
+        errorHandler(res, e);
+    }
 
 }
 
 module.exports.analytics = (req, res) => {
 
+}
+
+
+function getOrdersMap(orders= []) {
+    const daysOrders = {}; 
+    orders.forEach(order => {
+        const date = moment(order.date).format('DD.MM.YYYY');
+
+        if(date === moment().format('DD.MM.YYYY')) {
+            return;
+        }
+
+        if (!daysOrders[date]) {
+            daysOrders[date] = [];
+        }
+        daysOrders[date].push(order);
+    })
+    return daysOrders;
+};
+
+function calculatePrice(orders = []) {
+    return orders.reduce((acc, order) => {
+        const orderPrice = order.list.reduce((orderTotal, el) => {
+            return orderTotal += el.cost * el.quantity;
+        }, 0);
+        return acc += orderPrice;
+    }, 0)
 }
--- controllers/order.js
+++ controllers/order.js
@@ -5,22 +5,24 @@ module.exports.getAll = async (req, res) => {
     const query = {
         user: req.user.id,
     };
+
+    console.log(req.query.start);
 // date from the order creation
-    if (req.guery.start) {
+    if (req.guery?.start) {
         query.date = {
             $gte: req.query.start    //greatful or equall
         }
     };
 
     // date from the order creation
-    if (req.guery.end) {
+    if (req.guery?.end) {
         if (!query.date) {
             query.date = {};
         }
         query.date[$lte] = req.query.end;   //less or equall
     };
 
-    if (req.query.order) {
+    if (req.query?.order) {
         query.order = Number(req.query.order);
     };
 
@@ -31,7 +33,7 @@ module.exports.getAll = async (req, res) => {
             .skip(Number(req.query.offset))
             .limit(Number(req.query.limit));
         
-        res.status(200).json({ orders });
+        res.status(200).json(orders);
     } catch (e) {
         errorHandler(res,e);
     }
@@ -51,10 +53,7 @@ module.exports.create = async (req, res) => {
             order: maxOrder + 1, // number in order of all our orsers
             user: req.user.id,
         }).save()
-        res.status(201).json({
-            order,
-            message: 'order eas created',
-        })
+        res.status(201).json(order)
     } catch (e) {
         errorHandler(res,e);
     }
